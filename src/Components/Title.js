331dbc5ee58605c3240e6daf3f87a6641cd9d5d6
Joe <joe4ict@gmail.com>
2018-12-03 18:35:15 +0200

    Adding logo

--- src/Components/Title.js
+++ src/Components/Title.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
+import mainLogo from '../images/logo.png';
 
-const Title = () => (
-   <div>
-       <center className=".title-container__title">
-       <br/>
-            <h1> iWeather</h1>
-        </center>
-    </div>
-);
 
-export default Title;
+const styles = {
+  bigAvatar: {
+    margin: 0,
+    width: 80,
+    height: 80,
+  },
+};
+class Title extends React.Component{
+    render(){
+        const { classes } = this.props;
+        return (
+          <Grid container justify="center" alignItems="center">
+            <Avatar alt="logo" src={mainLogo} className={classes.bigAvatar} />
+            <h1>iWeather</h1>
+          </Grid>
+        );
+    }
+}
+
+export default  withStyles(styles)(Title); 
+
